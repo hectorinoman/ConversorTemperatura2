579e3026fd9b354d64b4ef32a8669866404b4b66
hectorinoman <hectorinoman@gmail.com>
2015-10-15 23:48:23 +0000

    herencia terminada

--- temperatura.js
+++ temperatura.js
@@ -1,28 +1,58 @@
-"use strict";
-
-function Medida(valor, tipo){
-  this.valor=valor; //valor numerico
-  this.tipo=tipo; //tipo cadena
-}
-
-function Temperatura(){
-}
-
-Temperatura.prototype = new Medida();
-
- 
+"use strict"; // Use ECMAScript 5 strict mode in browsers that support it
+function Medida(valor,tipo) {
+  
+  this.valor_=valor;
+  this.tipo_=tipo;
+    
  Medida.prototype.get_valor = function(){ 
-   return this.valor; 
+   return this.valor_; 
  }
 
  Medida.prototype.get_tipo = function(){ 
-   return this.tipo; 
+   return this.tipo_; 
  }
 
- Medida.prototype.set_valor = function(valor){ 
-   this.valor_=valor;
- }
+}
 
- Medida.prototype.set_tipo = function(tipo){ 
-   this.tipo_=tipo; 
- }
+function Temperatura (valor, tipo) {
+    Medida.call(this, valor, tipo);
+    var type= this.get_tipo();
+    var result;
+    
+
+    var number = parseFloat(this.get_valor());
+    if (type == 'c' || type == 'C') {
+      result = (number * 9/5)+32;
+      result = result.toFixed(1)+" Farenheit"
+    }
+    else {
+      result = (number - 32)*5/9;
+      result = result.toFixed(1)+" Celsius"
+    }
+    converted.innerHTML = result;
+
+}
+
+
+function calculate() {
+
+  var temp = original.value;
+  var regexp = /([+-]?\d+(?:\.\d*)?(?:\s*[e]\d+)?)\s*([fFcC])/;
+  
+  var m = temp.match(regexp);
+  
+  if (m) {
+    
+    var num = m[1]; // lo que cogio los parentesis
+    var type = m[2];  // lo que cogio los segundos parentesis si son celcius o farengeis
+    
+      Temperatura.prototype = new Medida;
+      a = new Temperatura(num,type);
+   
+  
+  }
+  else {
+    converted.innerHTML = "ERROR! Try something like '-4.2C' instead"; //no es una entrada valida 
+  }
+
+}
