d52ce733430554e449c3fdef86edcbdaab07097c
hectorinoman <hectorinoman@gmail.com>
2015-10-15 16:51:01 +0000

    ClaseTemperatura

--- temperatura.js
+++ temperatura.js
@@ -4,3 +4,25 @@ function Medida(valor, tipo){
   this.valor=valor; //valor numerico
   this.tipo=tipo; //tipo cadena
 }
+
+function Temperatura(){
+}
+
+Temperatura.prototype = new Medida();
+
+ 
+ Medida.prototype.get_valor = function(){ 
+   return this.valor; 
+ }
+
+ Medida.prototype.get_tipo = function(){ 
+   return this.tipo; 
+ }
+
+ Medida.prototype.set_valor = function(valor){ 
+   this.valor_=valor;
+ }
+
+ Medida.prototype.set_tipo = function(tipo){ 
+   this.tipo_=tipo; 
+ }
