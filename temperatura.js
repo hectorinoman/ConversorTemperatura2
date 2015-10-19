8ec50ef6c44a274de53eb5f94aab4f605e6c88fe
hectorinoman <hectorinoman@gmail.com>
2015-10-19 17:27:11 +0000

    clase modificada

--- temperatura.js
+++ temperatura.js
@@ -2,6 +2,7 @@
 function Medida(valor,tipo) {
   this.valor_=valor;
   this.tipo_=tipo;
+}
   Medida.prototype.get_valor = function(){ 
   return this.valor_; 
  }
@@ -10,47 +11,48 @@ function Medida(valor,tipo) {
    return this.tipo_; 
  }
 
-}
-
 function Temperatura (valor, tipo) {
     Medida.call(this, valor, tipo);
-    var type= this.get_tipo();
-    var result;
-    
-
-    var number = parseFloat(this.get_valor());
-    if (type == 'c' || type == 'C') {
-      result = (number * 9/5)+32;
-      result = result.toFixed(1)+" Farenheit"
-    }
-    else {
-      result = (number - 32)*5/9;
-      result = result.toFixed(1)+" Celsius"
-    }
-    converted.innerHTML = result;
-
 }
 
+ Temperatura.prototype = new Medida();
+ 
+ Medida.prototype.set_valor = function(valor){ this.valor_=valor; }
 
-function calculate() {
+ Medida.prototype.set_tipo = function(tipo){ this.tipo_=tipo; }
 
-  var temp = original.value;
-  var regexp = /([+-]?\d+(?:\.\d*)?(?:\s*[e]\d+)?)\s*([fFcC])/;
-  
-  var m = temp.match(regexp);
-  
-  if (m) {
-    
-    var num = m[1]; // lo que cogio los parentesis
-    var type = m[2];  // lo que cogio los segundos parentesis si son celcius o farengeis
-    
-      Temperatura.prototype = new Medida;
-      var a = new Temperatura(num,type);
-   
-  
-  }
-  else {
-    converted.innerHTML = "ERROR! Try something like '-4.2C' instead"; //no es una entrada valida 
-  }
+ Temperatura.prototype.Fahrenheit = function(){ return (this.get_valor()*9/5)+32; }
 
-}
+ Temperatura.prototype.Celsius = function(){ return (this.get_valor()-32)*5/9; }
+
+ function calculate(){
+     var result=new Temperatura();
+     var temp=original.value;
+
+     if(temp){
+       var regexp = /([-+]?\d+(?:\.\d*)?(?:[eE][+-]?\d+)?)\s*([fFcC])/;
+       var valor = temp.match(regexp);
+
+       if(valor){
+         var temp_=new Temperatura();
+
+         temp_.set_valor(parseFloat(valor[1]));
+         temp_.set_tipo(valor[2]);
+
+         if (temp_.get_tipo() == 'c' || temp_.get_tipo() == 'C') {
+           result.set_valor(temp_.Fahrenheit());
+           result.set_tipo("Fahrenheit");
+         }else {
+           result.set_valor(temp_.Celsius());
+           result.set_tipo("Celsius");
+         }
+         result = result.get_valor() + " " + result.get_tipo();
+         converted.innerHTML = result;
+       }
+       else {
+         converted.innerHTML = "ERROR! Pruebe algo asi '-4.2C' o '-4.2e15C'";
+       }
+     }
+   }
+
+
